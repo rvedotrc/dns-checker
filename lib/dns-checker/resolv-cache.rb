12136c04df4f9d35926bc77fe81ef20188c3ef84
Rachel Evans <git@rve.org.uk>
2015-01-08 08:31:12 +0000

    Check TTLs in cache

--- lib/dns-checker/resolv-cache.rb
+++ lib/dns-checker/resolv-cache.rb
@@ -11,17 +11,17 @@ module DNSChecker
     def get_answer(nameservers, zone, type)
 
       nameservers.each do |ns|
-        if answer = read_cache(ns, zone, type)
+        if retval = read_cache(ns, zone, type)
           puts "(cached)"
-          return answer
+          return retval
         end
       end
 
       nameservers.shuffle.each do |ns|
-        if answer = get_answer_lookup(ns, zone, type)
-          write_cache(ns, zone, type, answer)
+        if retval = get_answer_lookup(ns, zone, type)
+          write_cache(ns, zone, type, retval)
           puts "(fetched)"
-          return answer
+          return retval
         end
       end
 
@@ -32,9 +32,9 @@ module DNSChecker
     private
 
     def get_answer_lookup(ns, zone, type)
-      answer = nil
-      Resolv::DNS.new(:nameserver => [ns]).fetch_resource(zone, type) {|*args| answer = args}
-      answer
+      retval = nil
+      Resolv::DNS.new(:nameserver => [ns]).fetch_resource(zone, type) {|*args| retval = args}
+      retval
     end
 
     def read_cache(ns, zone, type, now = nil)
@@ -43,23 +43,45 @@ module DNSChecker
       rescue Errno::ENOENT
         return nil
       end
-      time, answer = Marshal.restore data
+      time, retval = Marshal.restore data
 
-      # TODO check TTLs; return nil if expired
-      # now ||= Time.now
+      p time
+      p retval
 
-      answer
+      # because retval = [ answer, name ]
+      if answer_expired?(retval[0], time, now)
+        puts "cached but expired"
+        return nil
+      end
+
+      retval
     end
 
-    def write_cache(ns, zone, type, answer, now = nil)
+    def write_cache(ns, zone, type, retval, now = nil)
       now ||= Time.now
-      data = Marshal.dump([ now, answer ])
+      data = Marshal.dump([ now, retval ])
       # TODO tmp + rename
       IO.write(cache_file(ns, zone, type), data)
     end
 
     def cache_file(ns, zone, type)
       "#{@dir}/#{ns}-#{zone.to_s}-#{type.name}"
+    end
+
+    def answer_expired?(answer, time, now)
+      now ||= Time.now
+      age = now - time
+
+      min_ttl = find_min_rr_ttl(answer)
+      age > min_ttl
+    end
+
+    def find_min_rr_ttl(answer)
+      %w[ answer authority additional ].map do |set|
+        answer.send(set.to_sym).map do |name, ttl, rr_with_ttl|
+          ttl
+        end
+      end.flatten.min || 0
     end
 
   end
